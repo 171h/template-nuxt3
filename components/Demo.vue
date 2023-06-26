24cca7fee20290e7100584f99abddaed10c12c67
H171 <1091088708@qq.com>
2023-06-26 11:10:40 +0800

    first commit

---
+++ components/Demo.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Test Pages Demo
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const appName = 'Template with Nuxt 3'
+export const appDescription = 'Template with Nuxt 3'
